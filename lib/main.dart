dbc027a64ead4f3754841a55bbd124884139f651
Amr Saied <120340317+AmrElskaan@users.noreply.github.com>
2023-11-15 10:44:40 +0200

    - initial data for test

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,18 @@ import 'amwal_sdk_settings/amwal_sdk_settings.dart';
 import 'localization/app_localizations_setup.dart';
 
 void main(List<String> args) {
+  if (args.isEmpty) {
+    args = [
+      "34750", // merchant id
+      "OMR", //currency Name
+      "512", // currency code
+      "20", // amount
+      "248046", // terminalId,
+      "0", // isMocked "1" for true and "0" for false
+      "X-Pharmacy", // merchant name
+      "40D6C01D0FA2460CD4DF29889C25C3E4DA96C61902AEF770E3D57C41F9D50FD7",
+    ];
+  }
   final settings = AmwalSdkSettings.fromArgs(args);
   runApp(MyApp(
     settings: settings,
