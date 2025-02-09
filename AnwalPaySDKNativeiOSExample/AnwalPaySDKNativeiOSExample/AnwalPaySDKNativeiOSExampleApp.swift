f381e2601f3a81865be7d4bd3b5819a85a0e2458
Amr Saied <Amr.elskaan@amwal-pay.com>
2025-02-09 17:00:54 +0100

    Refactor: Update pod configuration and SDK integration in iOS example
    This commit refactors the pod configuration and SDK integration in the iOS example project. It introduces a flag `compileFlutterModule` to switch between using a local Flutter module or a remote AmwalSDK pod. It also updates the SDK integration code by modifying the config value.
    
    Changes:
    - Added `compileFlutterModule` flag to Podfile for local/remote dependency choice.
    - Updated pod installation to use the flag for local Flutter module or remote AmwalSDK.
    - Modified the Swift code to manage config updates and SDK launch.
    - Added function startSdk to fetch the session and launch the SDK.
    - Added NavigationStack to manage navigation.

--- AnwalPaySDKNativeiOSExample/AnwalPaySDKNativeiOSExample/AnwalPaySDKNativeiOSExampleApp.swift
+++ AnwalPaySDKNativeiOSExample/AnwalPaySDKNativeiOSExample/AnwalPaySDKNativeiOSExampleApp.swift
@@ -11,33 +11,45 @@ import SwiftUI
 struct AnwalPaySDKNativeiOSExampleApp: App {
 
     private let networkClient = NetworkClient()
-
+    @State private var config: Config?
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            NavigationStack {
                 FormView(onSubmit:  { viewModel in
-                
-                    networkClient.fetchSessionToken(
-                        env: .UAT,
-                        merchantId: viewModel.merchantId,
-                        customerId: nil,
-                        secureHashValue: viewModel.secureHash
-                    ) { [self] sessionToken in
-                        if let token = sessionToken {
-                            
-                            print("Session token: \(token)")
-                            let config = Config(environment: viewModel.selectedEnv, sessionToken: token, currency: viewModel.currency, amount: viewModel.amount, merchantId: viewModel.merchantId, terminalId: viewModel.terminalId, locale: viewModel.language, isSoftPOS: viewModel.transactionType == .NFC)
-                            
-                            SDKViewControllerRepresentable(config: config,onResponse: {_ in },onCustomerId: {_ in })
-                            
-                            
-                        }else {
-                            print("Failed to fetch session token.")
-                        }
-                    }
-                    
+                    startSdk(viewModel: viewModel)
                 })
+                .navigationDestination(isPresented: Binding<Bool>(
+                    get: { config != nil },
+                    set: { if !$0 { config = nil } }
+                )) {
+                                   if let config = config {
+                                       SDKViewControllerRepresentable(
+                                           config: config,
+                                           onResponse: { _ in },
+                                           onCustomerId: { _ in }
+                                       )
+                                   }
+                               }
+            }
+        }
+    }
+    
+    func startSdk(viewModel: PaymentFormViewModel) {
+        networkClient.fetchSessionToken(
+            env: .UAT,
+            merchantId: viewModel.merchantId,
+            customerId: nil,
+            secureHashValue: viewModel.secureHash
+        ) { [self] sessionToken in
+            if let token = sessionToken {
+                
+                print("Session token: \(token)")
+                config = Config(environment: viewModel.selectedEnv, sessionToken: token, currency: viewModel.currency, amount: viewModel.amount, merchantId: viewModel.merchantId, terminalId: viewModel.terminalId, locale: viewModel.language, isSoftPOS: viewModel.transactionType == .NFC)
+                                
+                
+            }else {
+                print("Failed to fetch session token.")
             }
         }
     }
