255bc32fab17afdc8431b346d4c988e0e8967ca7
Amr Saied <Amr.elskaan@amwal-pay.com>
2025-02-13 10:16:24 +0100

    Update Android package name and enable local module
    This commit includes the following changes:
    *   Updated the Android package name in `pubspec.yaml` from `com.example.amwal_sdk_flutter_module` to `com.amwal-pay.flutter`.
    *   Updated the iOS bundle identifier in `pubspec.yaml` from `com.example.amwalSdkFlutterModule` to `com.amwal-pay.flutter`.
    * Updated `amwalsdk/build.gradle.kts` to match the new package name
    *   Enabled local module repository in `settings.gradle.kts`.
    * Add maven publishing plugin and config it.

--- AnwalPaySDKNativeExample/amwalsdk/build.gradle.kts
+++ AnwalPaySDKNativeExample/amwalsdk/build.gradle.kts
@@ -1,9 +1,12 @@
-import org.gradle.internal.extensions.core.extra
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 android {
@@ -46,11 +49,56 @@ dependencies {
     if (compileFlutterModule) {
         implementation(project(":flutter"))
     }else{
-        debugImplementation("com.example.amwal_sdk_flutter_module:flutter_debug:1.0")
-        add("profileImplementation", "com.example.amwal_sdk_flutter_module:flutter_profile:1.0")
-        releaseImplementation("com.example.amwal_sdk_flutter_module:flutter_release:1.0")
+        debugImplementation("com.amwal-pay.flutter:flutter_debug:1.0")
+        add("profileImplementation", "com.amwal-pay.flutter:flutter_profile:1.0")
+        releaseImplementation("com.amwal-pay.flutter:flutter_release:1.0")
     }
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    configure(AndroidSingleVariantLibrary(
+        // the published variant
+        variant = "release",
+        // whether to publish a sources jar
+        sourcesJar = false,
+        // whether to publish a javadoc jar
+        publishJavadocJar = true,
+    ))
+
+    coordinates("com.amwal-pay", "amwal-sdk", "1.0.0")
+
+    // the following is optional
+
+    pom {
+        name.set("Amwal SDK")
+        description.set("A sample SDK for Amwal Pay.")
+        inceptionYear.set("2024")
+        url.set("https://amwal-pay.com")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("amr.elskaan")
+                name.set("Amr Said")
+                url.set("amr.elskaan@amwal-pay.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/username/mylibrary/")
+            connection.set("scm:git:git://github.com/username/mylibrary.git")
+            developerConnection.set("scm:git:ssh://git@github.com/username/mylibrary.git")
+        }
+    }
 }
