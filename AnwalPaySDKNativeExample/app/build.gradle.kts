9055c8d8228bf4cf43f85f1cec82ec865146bd7f
Amr Saied <amr.elskaan@amwal-pay.com>
2025-04-15 18:10:59 +0200

    update version

--- AnwalPaySDKNativeExample/app/build.gradle.kts
+++ AnwalPaySDKNativeExample/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(project(":amwalsdk"))
     implementation(libs.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
